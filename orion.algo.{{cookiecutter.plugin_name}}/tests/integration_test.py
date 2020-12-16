41dc1c89d1bbdcc27bf3bcbd6c7c6af17d492f82
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-12-16 16:04:14 -0500

    Improve set of tests

--- orion.algo.{{cookiecutter.plugin_name}}/tests/integration_test.py
+++ orion.algo.{{cookiecutter.plugin_name}}/tests/integration_test.py
@@ -1,44 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Perform integration tests for `orion.algo.skopt`."""
+# pylint:disable=invalid-name
+"""Perform integration tests for `orion.algo.{{ cookiecutter.plugin_name }}`."""
 import os
 
 import numpy
-import pytest
-
-from orion.algo.space import Integer, Real, Space
 import orion.core.cli
-from orion.core.io.experiment_builder import ExperimentBuilder
+import pytest
+from orion.algo.space import Integer, Real, Space
+from orion.client import create_experiment
 from orion.core.utils.tests import OrionState
 from orion.core.worker.primary_algo import PrimaryAlgo
 
 
-@pytest.fixture(scope='session')
-def database():
-    """Return Mongo database object to test with example entries."""
-    from pymongo import MongoClient
-    client = MongoClient(username='user', password='pass', authSource='orion_test')
-    database = client.orion_test
-    yield database
-    client.close()
-
-
-@pytest.fixture()
-def clean_db(database):
-    """Clean insert example experiment entries to collections."""
-    database.experiments.drop()
-    database.trials.drop()
-    database.workers.drop()
-    database.resources.drop()
+# pylint:disable=unused-argument
+def rosenbrock_function(x, y):
+    """Evaluate a n-D rosenbrock function."""
+    z = x - 34.56789
+    r = 4 * z ** 2 + 23.4
+    return [dict(name="objective", type="objective", value=r)]
 
 
 @pytest.fixture()
 def space():
     """Return an optimization space"""
     space = Space()
-    dim1 = Integer('yolo1', 'uniform', -3, 6)
+    dim1 = Integer("yolo1", "uniform", -3, 6)
     space.register(dim1)
-    dim2 = Real('yolo2', 'uniform', 0, 1)
+    dim2 = Real("yolo2", "uniform", 0, 1)
     space.register(dim2)
 
     return space
@@ -46,39 +35,46 @@ def space():
 
 def test_seeding(space):
     """Verify that seeding makes sampling deterministic"""
-    optimizer = PrimaryAlgo(space, '{{ cookiecutter.algo_name|lower }}')
+    optimizer = PrimaryAlgo(space, "{{ cookiecutter.algo_name|lower }}")
 
     optimizer.seed_rng(1)
     a = optimizer.suggest(1)[0]
-    assert not numpy.allclose(a, optimizer.suggest(1)[0])
+    with pytest.raises(AssertionError):
+        numpy.testing.assert_equal(a, optimizer.suggest(1)[0])
 
     optimizer.seed_rng(1)
-    assert numpy.allclose(a, optimizer.suggest(1)[0])
+    numpy.testing.assert_equal(a, optimizer.suggest(1)[0])
 
 
 def test_set_state(space):
     """Verify that resetting state makes sampling deterministic"""
-    optimizer = PrimaryAlgo(space, '{{ cookiecutter.algo_name|lower }}')
+    optimizer = PrimaryAlgo(space, "{{ cookiecutter.algo_name|lower }}")
 
     optimizer.seed_rng(1)
     state = optimizer.state_dict
     a = optimizer.suggest(1)[0]
-    assert not numpy.allclose(a, optimizer.suggest(1)[0])
+    with pytest.raises(AssertionError):
+        numpy.testing.assert_equal(a, optimizer.suggest(1)[0])
 
     optimizer.set_state(state)
-    assert numpy.allclose(a, optimizer.suggest(1)[0])
+    numpy.testing.assert_equal(a, optimizer.suggest(1)[0])
 
 
-def test_optimizer(monkeypatch):
+def test_optimizer_basic(monkeypatch):
     """Check functionality of BayesianOptimizer wrapper for single shaped dimension."""
     monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     with OrionState(experiments=[], trials=[]):
 
-        orion.core.cli.main(["hunt", "--name", "exp", "--max-trials", "5", "--config",
-                             "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
-                             "./benchmark/rosenbrock.py",
-                             "-x~uniform(-5, 5)"])
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~uniform(-5, 5)",
+            ]
+        )
 
 
 def test_int(monkeypatch):
@@ -87,10 +83,15 @@ def test_int(monkeypatch):
 
     with OrionState(experiments=[], trials=[]):
 
-        orion.core.cli.main(["hunt", "--name", "exp", "--max-trials", "5", "--config",
-                             "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
-                             "./benchmark/rosenbrock.py",
-                             "-x~uniform(-5, 5, discrete=True)"])
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~uniform(-5, 5, discrete=True)",
+            ]
+        )
 
 
 def test_categorical(monkeypatch):
@@ -99,10 +100,49 @@ def test_categorical(monkeypatch):
 
     with OrionState(experiments=[], trials=[]):
 
-        orion.core.cli.main(["hunt", "--name", "exp", "--max-trials", "5", "--config",
-                             "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
-                             "./benchmark/rosenbrock.py",
-                             "-x~choices([-5, -2, 0, 2, 5])"])
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~choices([-5, -2, 0, 2, 5])",
+            ]
+        )
+
+
+def test_linear(monkeypatch):
+    """Check support of logarithmic distributions."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    with OrionState(experiments=[], trials=[]):
+
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~loguniform(1, 50, discrete=True)",
+            ]
+        )
+
+
+def test_shape(monkeypatch):
+    """Check support of multidim values."""
+    monkeypatch.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    with OrionState(experiments=[], trials=[]):
+
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~uniform(-5, 5, shape=3)",
+            ]
+        )
 
 
 def test_optimizer_two_inputs(monkeypatch):
@@ -111,10 +151,16 @@ def test_optimizer_two_inputs(monkeypatch):
 
     with OrionState(experiments=[], trials=[]):
 
-        orion.core.cli.main(["hunt", "--name", "exp", "--max-trials", "5", "--config",
-                             "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
-                             "./benchmark/rosenbrock.py",
-                             "-x~uniform(-5, 5)", "-y~uniform(-10, 10)"])
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "-x~uniform(-5, 5)",
+                "-y~uniform(-10, 10)",
+            ]
+        )
 
 
 def test_optimizer_actually_optimize(monkeypatch):
@@ -124,15 +170,20 @@ def test_optimizer_actually_optimize(monkeypatch):
 
     with OrionState(experiments=[], trials=[]):
 
-        orion.core.cli.main(["hunt", "--name", "exp", "--max-trials", "20", "--config",
-                             "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
-                             "./benchmark/rosenbrock.py",
-                             "-x~uniform(-50, 50)"])
+        orion.core.cli.main(
+            [
+                "hunt",
+                "--config",
+                "./benchmark/{{ cookiecutter.algo_name|lower }}.yaml",
+                "./benchmark/rosenbrock.py",
+                "--max-trials",
+                100,
+                "-x~uniform(-50, 50)",
+            ]
+        )
 
-        with open("./benchmark/{{ cookiecutter.algo_name|lower }}.yaml", "r") as f:
-            exp = ExperimentBuilder().build_view_from(
-                {'name': 'exp', 'config': f})
+        exp = create_experiment(name="exp")
 
-        objective = exp.stats['best_evaluation']
+        objective = exp.stats["best_evaluation"]
 
         assert best_random_search > objective
